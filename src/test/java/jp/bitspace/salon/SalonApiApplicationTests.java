4f3d71be32ce2fef4c453b78216d56ba42298973
s.ohura <ohura@bitspace.jp>
2025-11-02 18:28:54 +0900

    初回コミット

---
+++ src/test/java/jp/bitspace/salon/SalonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.bitspace.salon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
