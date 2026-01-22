d6c5344a3681384a29570a59b71b721ca14fc66d
s.ohura <ohura@bitspace.jp>
2026-01-22 20:57:35 +0900

    ヘルスチェック追加

---
+++ src/main/java/jp/bitspace/salon/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package jp.bitspace.salon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
