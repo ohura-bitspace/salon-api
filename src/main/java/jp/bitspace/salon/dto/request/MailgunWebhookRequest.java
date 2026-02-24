f05d2b59bbf720f946c7e906bc1b756527ec7e1c
s.ohura <ohura@bitspace.jp>
2026-02-24 23:45:07 +0900

    @JsonProperty追加

--- src/main/java/jp/bitspace/salon/dto/request/MailgunWebhookRequest.java
+++ src/main/java/jp/bitspace/salon/dto/request/MailgunWebhookRequest.java
@@ -1,6 +1,9 @@
 package jp.bitspace.salon.dto.request;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Mailgun Webhook リクエスト.
@@ -11,7 +14,8 @@ import lombok.Data;
  *
  * @see <a href="https://documentation.mailgun.com/docs/mailgun/api-reference/openapi-final/tag/Webhooks/">Mailgun Webhooks</a>
  */
-@Data
+@Getter
+@Setter
 public class MailgunWebhookRequest {
 
     // ── 送信者・受信者情報 ──────────────────────────
@@ -32,18 +36,23 @@ public class MailgunWebhookRequest {
 
     // ── 本文 ──────────────────────────────────────
     /** プレーンテキスト本文 (stripped-text) */
+    @JsonProperty("body-plain")
     private String bodyPlain;
 
     /** HTML 本文 (stripped-html) */
+    @JsonProperty("body-html")
     private String bodyHtml;
 
     /** HTML タグを除去した本文 */
+    @JsonProperty("stripped-text")
     private String strippedText;
 
     /** 引用部分を除去した HTML */
+    @JsonProperty("stripped-html")
     private String strippedHtml;
-
+    
     /** 署名を除去した本文 */
+    @JsonProperty("stripped-signature")
     private String strippedSignature;
 
     // ── Mailgun 署名検証用 ─────────────────────────
@@ -65,43 +74,5 @@ public class MailgunWebhookRequest {
 
     /** Content-Type ヘッダ */
     private String contentType;
-
-    // ── Mailgun のハイフン付きパラメータ名に対応 ────────
-    // Spring の DataBinder は "body-plain" を "body_plain" として処理するため、
-    // アンダースコア付きの setter を用意することで自動マッピングが可能になる。
-
-    /** Mailgun の "body-plain" パラメータをマッピング */
-    public void setBody_plain(String bodyPlain) {
-        this.bodyPlain = bodyPlain;
-    }
-
-    /** Mailgun の "body-html" パラメータをマッピング */
-    public void setBody_html(String bodyHtml) {
-        this.bodyHtml = bodyHtml;
-    }
-
-    /** Mailgun の "stripped-text" パラメータをマッピング */
-    public void setStripped_text(String strippedText) {
-        this.strippedText = strippedText;
-    }
-
-    /** Mailgun の "stripped-html" パラメータをマッピング */
-    public void setStripped_html(String strippedHtml) {
-        this.strippedHtml = strippedHtml;
-    }
-
-    /** Mailgun の "stripped-signature" パラメータをマッピング */
-    public void setStripped_signature(String strippedSignature) {
-        this.strippedSignature = strippedSignature;
-    }
-
-    /** Mailgun の "Message-Id" パラメータをマッピング */
-    public void setMessage_Id(String messageId) {
-        this.messageId = messageId;
-    }
-
-    /** Mailgun の "content-type" パラメータをマッピング */
-    public void setContent_type(String contentType) {
-        this.contentType = contentType;
-    }
+    
 }
